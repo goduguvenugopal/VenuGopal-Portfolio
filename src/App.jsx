9b99ef90d99b3224556ba7ae0a9060351638357d
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-03-02 15:07:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-blue-600 h-screen text-2xl text-white flex justify-center flex-col items-center p-3'>
+        <h5 className='mb-4 text-3xl bg-black p-2 rounded-2xl'>Godugu Venu Gopal</h5>
+        My website is currently under development. Stay tuned for updates!
+      </div>
+    </>
+  )
+}
+
+export default App
