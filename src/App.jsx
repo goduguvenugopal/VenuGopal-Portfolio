7d784f18ae6cbee17c2fa9523f390c92bf2e8004
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-03-02 15:55:14 +0530

    added meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <>
-      <div className='bg-blue-600 h-screen text-2xl text-white flex justify-center flex-col items-center p-3'>
+      <div className='bg-blue-600 text-center h-screen text-2xl text-white flex justify-center flex-col items-center p-3'>
+        <img className='w-24 mb-3 rounded-full' src="/logo.jpeg" alt="logo" />
         <h5 className='mb-4 text-3xl bg-black p-2 rounded-2xl'>Godugu Venu Gopal</h5>
         My website is currently under development. Stay tuned for updates!
       </div>
