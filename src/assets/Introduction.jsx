f00f8a660314eed4706416c18fdaca2ef388c62c
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-04-15 16:23:09 +0530

    added contextmenu for image to not download image

--- src/assets/Introduction.jsx
+++ src/assets/Introduction.jsx
@@ -5,6 +5,20 @@ import { Typewriter } from 'react-simple-typewriter'
 
 const Introduction = () => {
 
+  useEffect(() => {
+    const handleContextMenu = (e) => {
+      if (e.target.tagName === 'IMG') {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener('contextmenu', handleContextMenu);
+
+    // Cleanup on component unmount
+    return () => {
+      document.removeEventListener('contextmenu', handleContextMenu);
+    };
+  }, []);
 
   return (
     <>
@@ -14,7 +28,7 @@ const Introduction = () => {
         <div className='w-full border-2 border-purple-500 rounded-full order-1 lg:order-2 lg:w-[30%] lg:h-fit overflow-hidden'>
           <img src="/venugopal.jpg" alt="venugopal-image" className=' transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 rounded-full w-full h-fit' />
         </div>
-     
+
 
 
         {/* bio data and description section */}
@@ -40,10 +54,10 @@ const Introduction = () => {
           <p className='text-[#9ea2a3]  text-[1.11rem] '>{bioData.description}</p>
           <div className='d-flex items-center justify-between flex-wrap  mt-5'>
 
-          <a className='cursor-pointer' href={bioData.resume}><button className='rounded-full  lg:mr-5 cursor-pointer text-[1.2rem] h-[3rem]  text-white font-medium bg-gradient-to-r from-pink-500 w-[60%] lg:w-[40%] to-purple-600 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>View Resume</button>
-          </a>
-          <a className='cursor-pointer' href={`tel:+91${bioData.mobile}`}><button className='rounded-full mt-5 lg:mt-0 cursor-pointer text-[1.2rem] h-[3rem] text-white font-medium bg-gradient-to-l from-green-500 w-[60%] lg:w-[40%] to-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>Contact Us</button>
-          </a>
+            <a className='cursor-pointer' href={bioData.resume}><button className='rounded-full  lg:mr-5 cursor-pointer text-[1.2rem] h-[3rem]  text-white font-medium bg-gradient-to-r from-pink-500 w-[60%] lg:w-[40%] to-purple-600 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>View Resume</button>
+            </a>
+            <a className='cursor-pointer' href={`tel:+91${bioData.mobile}`}><button className='rounded-full mt-5 lg:mt-0 cursor-pointer text-[1.2rem] h-[3rem] text-white font-medium bg-gradient-to-l from-green-500 w-[60%] lg:w-[40%] to-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110'>Contact Us</button>
+            </a>
 
           </div>
         </div>
